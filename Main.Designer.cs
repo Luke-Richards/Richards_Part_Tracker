7c70717bb4fea77c4a062cedaf56600ced5b9427
silkyrie <32624451+silkyrie@users.noreply.github.com>
2023-02-24 19:43:27 -0500

    Add project files.

---
+++ Main.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+using System.Windows.Forms;
+
+namespace Richards_Part_Tracker
+{
+    partial class PartTracker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PartTracker));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.viewPartTracker = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.AutoScroll = true;
+            this.splitContainer1.Panel1.Controls.Add(this.txtSearch);
+            this.splitContainer1.Panel1.Controls.Add(this.btnAdd);
+            this.splitContainer1.Panel1.Padding = new System.Windows.Forms.Padding(20);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.splitContainer1.Panel2.Controls.Add(this.viewPartTracker);
+            this.splitContainer1.Panel2.Padding = new System.Windows.Forms.Padding(10, 0, 10, 10);
+            this.splitContainer1.Size = new System.Drawing.Size(430, 624);
+            this.splitContainer1.SplitterDistance = 202;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(10, 151);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(193, 28);
+            this.txtSearch.TabIndex = 4;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(10, 117);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(159, 28);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add New Part";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // viewPartTracker
+            // 
+            this.viewPartTracker.BackColor = System.Drawing.SystemColors.Window;
+            this.viewPartTracker.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.viewPartTracker.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewPartTracker.FullRowSelect = true;
+            this.viewPartTracker.GridLines = true;
+            this.viewPartTracker.HideSelection = false;
+            this.viewPartTracker.Location = new System.Drawing.Point(10, 0);
+            this.viewPartTracker.Name = "viewPartTracker";
+            this.viewPartTracker.Size = new System.Drawing.Size(410, 408);
+            this.viewPartTracker.TabIndex = 0;
+            this.viewPartTracker.UseCompatibleStateImageBehavior = false;
+            this.viewPartTracker.View = System.Windows.Forms.View.Details;
+            this.viewPartTracker.Resize += new System.EventHandler(this.viewPartTracker_Resize);
+            // 
+            // PartTracker
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(430, 624);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PartTracker";
+            this.Text = "Richards Part Tracker";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView viewPartTracker;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtSearch;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Richards_Part_Tracker
+{
+    public partial class PartTracker : Form
+    {
+        public PartTracker()
+        {
+            InitializeComponent();
+            viewPartTracker.Columns.Add("Part Name", -2, HorizontalAlignment.Left);
+            viewPartTracker.Columns.Add("Bin #", -2, HorizontalAlignment.Left);
+            viewPartTracker.Columns.Add("Quantity", -2, HorizontalAlignment.Left);
+            viewPartTracker.Columns.Add("Description", -2, HorizontalAlignment.Left);
+
+        }
+
+        private void splitContainer2_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string[] row = { "hello", "part", "location" };
+            viewPartTracker.Items.Add(new ListViewItem(row));
+
+        }
+
+
+        private void create_config()
+        {
+            string path = "./Richards_Part_Tracker.exe.config";
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+                File.WriteAllText(path, "<?xml version=\"1.0\" encoding=\"utf - 8\" ?>< configuration >< startup >< supportedRuntime version = \"v4.0\" sku = \".NETFramework,Version=v4.7.2\" /></ startup >< System.Windows.Forms.ApplicationConfigurationSection >< add key = \"DpiAwareness\" value = \"PerMonitorV2\" /></ System.Windows.Forms.ApplicationConfigurationSection ></ configuration > ");
+            }
+        }
+
+        private void viewPartTracker_Resize(object sender, EventArgs e)
+        {
+            //stops the "overflow/extra column" when the form is resized
+            foreach (ColumnHeader col in viewPartTracker.Columns)
+            {
+                col.Width = -2;
+            }
+        }
+    }
+}
